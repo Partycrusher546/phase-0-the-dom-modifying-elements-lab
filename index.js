c6e3ade8f8b3b6711c29e1dcc69fa32b99fb768f
partycrusher546 <muthonidennis546@gmail.com>
2024-03-31 00:00:32 +0300

    complete task

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Write your code here! 
+const elementToRemove = document.getElementById('main')
+const parentElement = elementToRemove.parentNode
+parentElement.removeChild(elementToRemove)
+
+var newHeader = document.createElement("h1")
+newHeader.textContent = 'this is a new header'
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+
+var newHeader = document.createElement("h1")
+newHeader.textContent = `${'Dennis'}is the champion`
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+
